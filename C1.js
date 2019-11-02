641bf99a80920f9416c7cbf63b37a2dbcc9c1a79
Wilton Ngo <wiltonngo@gmail.com>
2019-11-02 09:44:34 -0400

    remove comments

--- C1.js
+++ C1.js
@@ -1,7 +1,3 @@
 const doorToDoor = (volunteers, neighbourhoods) => {
-    // Code here!
     return neighbourhoods.length / volunteers.length;
-    // Remember to return a value
-
-    // Want to see what's going on inside your function? You can use console.log() to debug and print variables to the console below.
 }
