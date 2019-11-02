8067cd21c9bd64f1123d404cf8867225ee432150
Wilton Ngo <wiltonngo@gmail.com>
2019-11-01 23:54:50 -0400

    add solution to c1

---
+++ C1.js
@@ -0,0 +1,7 @@
+const doorToDoor = (volunteers, neighbourhoods) => {
+    // Code here!
+    return neighbourhoods.length / volunteers.length;
+    // Remember to return a value
+
+    // Want to see what's going on inside your function? You can use console.log() to debug and print variables to the console below.
+}
